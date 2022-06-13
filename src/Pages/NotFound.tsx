dcd895383f128139b64c5f3b49cdf71f29671365
m1xa4kala <maikl2020011@gmail.com>
2022-06-13 22:39:28 +0300

    refactor not found page

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,9 +1,16 @@
+import { Box, Heading, Link } from '@chakra-ui/react'
+import { Link as RouterLink } from 'react-router-dom'
 import React from 'react'
 
 const NotFound: React.FC = () => {
-  return (
-    <div>NotFound</div>
-  )
+	return (
+		<Box>
+			<Heading>Here is no Todos</Heading>
+			<Link as={RouterLink} to="/" color="blue.400">
+				Back to work
+			</Link>
+		</Box>
+	)
 }
 
 export default NotFound
