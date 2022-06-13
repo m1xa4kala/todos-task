c4bed95829137ceba114ab32479620a4017b5b88
m1xa4kala <maikl2020011@gmail.com>
2022-06-13 22:38:39 +0300

    refactor test-utils

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,12 +1,28 @@
-import * as React from "react"
-import { render, RenderOptions } from "@testing-library/react"
-import { ChakraProvider, theme } from "@chakra-ui/react"
+import * as React from 'react'
+import { render as rtlRender, RenderOptions } from '@testing-library/react'
+import { ChakraProvider, theme } from '@chakra-ui/react'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import todosReducer, { Filter, ITodosState } from './store/todosSlice'
+import { RootState } from './store'
 
-const AllProviders = ({ children }: { children?: React.ReactNode }) => (
-  <ChakraProvider theme={theme}>{children}</ChakraProvider>
-)
+function render(
+	ui: React.ReactElement,
+	preloadedState: RootState = {todos: {todos: [], currentFilter: Filter.ALL}},
+	store = configureStore({
+		reducer: { todos: todosReducer },
+		preloadedState,
+	}),
+	options?: RenderOptions
+) {
+	const AllProviders = ({ children }: { children?: React.ReactNode }) => (
+		<ChakraProvider theme={theme}>
+			<Provider store={store}>{children}</Provider>
+		</ChakraProvider>
+	)
+	return rtlRender(ui, { wrapper: AllProviders, ...options })
+}
 
-const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
-  render(ui, { wrapper: AllProviders, ...options })
+export * from '@testing-library/react'
 
-export { customRender as render }
+export { render }
